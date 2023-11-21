ee4c7c2cd75cbb9a7bda626969fda727cffb6e8e
Chaitan <chaitanr9915@gmail.com>
2023-11-21 16:58:45 +0530

    terraform_files

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "local_file" "my_pet" {
+    filename = var.filename
+    content = var.content
+    
+}
+resource "random_pet" "petname" {
+    prefix = "MR"
+    separator = "."
+    length = var.length
+
+  
+}
---
+++ variable.tf
@@ -0,0 +1,15 @@
+    variable "filename" {
+        default = "pet.txt"
+        type = string
+      
+    }
+    variable "content" {
+        default  = "dog is my favourite pet"
+        type = string
+      
+    }
+    variable "length" {
+        default = "1"
+        type = number
+      
+    }
