4c00ddd73c94cc57cc77e2ff01de7f2acd1f336c
Chaitan <chaitanr9915@gmail.com>
2023-11-22 20:10:38 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
 resource "local_file" "my_pet" {
-    filename = var.filename
-    content = var.content
-    
+    filename = "wild.txt"
+    content = "Lion is a wild animal I named him as ${random_pet.petname.id}"
+
 }
+    
+    output "my_pet" {
+        value = random_pet.petname.id
+    }
+
 resource "random_pet" "petname" {
     prefix = "MR"
     separator = "."
-    length = var.length
+    length = "1"
 
   
 }
